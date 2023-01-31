ad1ce1f2be5473a2ee623884968c89c6bbc5a3a2
Masaki Furuta <masaki.furuta@gmail.com>
2023-01-31 19:23:23 +0900

    Modify run_snc.sh

--- run_snc.sh
+++ run_snc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xv
 
+# Reference: https://github.com/crc-org/snc
+#            https://github.com/openshift/installer/blob/master/docs/dev/libvirt/README.md#one-time-setup
+
 echo 3 | sudo tee /proc/sys/vm/drop_caches
 
 # Before running ./snc.sh, you need to create a pull secret file, and set a couple of environment variables to override the default behavior.
@@ -9,14 +12,17 @@ echo 3 | sudo tee /proc/sys/vm/drop_caches
 # --
 
 # Create a pull secret file
+# Get pull secret from the following and set it to /tmp/pull_secret.json
+# https://console.redhat.com/openshift/create/local
 
 cat << EOF > /tmp/pull_secret.json
-{"auths":{"fake":{"auth": "Zm9vOmJhcgo="}}}
+<COPY AND PASTE SECRET FROM ABOVE URL>
 EOF
 
 # Set environment for OKD build
 export OKD_VERSION=4.10.0-0.okd-2022-07-09-073606
 export OKD_VERSION=4.10.0-0.okd-2022-06-10-131327
+export OKD_VERSION=4.12.0-0.okd-2023-01-21-055900
 export OPENSHIFT_PULL_SECRET_PATH="/tmp/pull_secret.json"
 
 cd ~/snc || exit 1
